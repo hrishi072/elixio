311605d7bec85352205480612096575d24241910
hrishi072 <parundekarrushi@gmail.com>
2025-01-21 03:31:28 +0530

    Views file formated

--- users/views.py
+++ users/views.py
@@ -1,49 +1,44 @@
-from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth import (
-    REDIRECT_FIELD_NAME, authenticate, login as auth_login,
-    logout as auth_logout,
-)
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-from django.shortcuts import get_object_or_404, redirect, render, resolve_url
-from django.template.response import TemplateResponse
-from django.utils.decorators import method_decorator
-from django.utils.http import is_safe_url
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import ListView
-
-import requests
 
 from mysite.decorators import ajax_required
 from notifications.models import Notification
 from subjects.models import Subject
 from utils import check_image_extension
 
+from .decorattors import unauthenticated_user
 from .forms import ProfileEditForm, SignupForm, UserEditForm
 from .models import Profile
-from .decorattors import unauthenticated_user
+
 
 @unauthenticated_user
 def register_user(request):
     form_filling = True
-    if request.method == 'POST':
+    if request.method == "POST":
         user_form = SignupForm(data=request.POST or None)
         if not user_form.is_valid():
-            return render(request, 'registration/register.html', {
-                'user_form': user_form, 'form_filling': form_filling
-            })
+            return render(
+                request,
+                "registration/register.html",
+                {"user_form": user_form, "form_filling": form_filling},
+            )
         else:
             new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.set_password(user_form.cleaned_data["password"])
             new_user.save()
             auth = authenticate(
-                username=user_form.cleaned_data['username'],
-                password=user_form.cleaned_data['password'],
-                )
+                username=user_form.cleaned_data["username"],
+                password=user_form.cleaned_data["password"],
+            )
             auth_login(request, auth)
             msg_txt = """
                     <h4 class="alert-heading" >
@@ -61,23 +56,23 @@ def register_user(request):
                     </p>
                 """
             messages.success(request, msg_txt)
-            return redirect('/')
+            return redirect("/")
     else:
         user_form = SignupForm()
-    return render(request, 'registration/register.html', {
-        'user_form': user_form, 'form_filling': form_filling
-    })
-    
-    
+    return render(
+        request,
+        "registration/register.html",
+        {"user_form": user_form, "form_filling": form_filling},
+    )
+
+
 @ajax_required
 def check_username(request):
     """
     Ajax call to check username availability.
     """
-    username = request.GET.get('username', None)
-    data = {
-        'is_taken': User.objects.filter(username__iexact=username).exists()
-    }
+    username = request.GET.get("username", None)
+    data = {"is_taken": User.objects.filter(username__iexact=username).exists()}
     return JsonResponse(data)
 
 
@@ -87,17 +82,18 @@ def user_logout(request):
     Logout the current user.
     """
     auth_logout(request)
-    return redirect('login')
+    return redirect("login")
 
 
 class UsersPageView(ListView):
     """
     Basic ListView implementation to call the users list.
     """
+
     model = User
     paginate_by = 20
-    template_name = 'users/view_all_users.html'
-    context_object_name = 'users'
+    template_name = "users/view_all_users.html"
+    context_object_name = "users"
 
     def get_queryset(self, **kwargs):
         return User.objects.exclude(username=self.request.user.username)
@@ -107,10 +103,11 @@ class FollowersPageView(LoginRequiredMixin, ListView):
     """
     Basic ListView implementation to call the followers list per user.
     """
+
     model = User
     paginate_by = 20
-    template_name = 'users/followers.html'
-    context_object_name = 'users'
+    template_name = "users/followers.html"
+    context_object_name = "users"
 
     def get_queryset(self, **kwargs):
         return self.request.user.profile.followers.all()
@@ -120,10 +117,11 @@ class FollowingPageView(LoginRequiredMixin, ListView):
     """
     Basic ListView implementation to call the following list per user.
     """
+
     model = User
     paginate_by = 20
-    template_name = 'users/following.html'
-    context_object_name = 'profiles'
+    template_name = "users/following.html"
+    context_object_name = "profiles"
 
     def get_queryset(self, **kwargs):
         return self.request.user.following.all()
@@ -135,19 +133,16 @@ def follow_user(request, user_id):
     """
     Ajax call to follow a user.
     """
-    user = get_object_or_404(User,
-                             id=user_id)
+    user = get_object_or_404(User, id=user_id)
     if request.user in user.profile.followers.all():
         user.profile.followers.remove(request.user)
-        text = 'Follow'
+        text = "Follow"
     else:
         user.profile.followers.add(request.user)
         Notification.objects.create(
-            Actor=request.user,
-            Target=user,
-            notif_type='follow'
+            Actor=request.user, Target=user, notif_type="follow"
         )
-        text = 'Unfollow'
+        text = "Unfollow"
     return HttpResponse(text)
 
 
@@ -156,11 +151,9 @@ def profile_edit(request):
     """
     Handles user profile edit action.
     """
-    if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user,
-                                 data=request.POST)
-        profile_form = ProfileEditForm(instance=request.user.profile,
-                                       data=request.POST)
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST)
         if user_form.is_valid():
             user_form.save()
         else:
@@ -176,15 +169,16 @@ def profile_edit(request):
         """
 
         messages.success(request, msg_txt)
-        return redirect('profile_edit')
+        return redirect("profile_edit")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
 
-    return render(request, 'users/profile_edit.html', {
-        'user_form': user_form,
-        'profile_form': profile_form
-    })
+    return render(
+        request,
+        "users/profile_edit.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
 
 
 @login_required
@@ -192,8 +186,8 @@ def change_picture(request):
     """
     Handles profile picture change action.
     """
-    if request.method == 'POST':
-        user_dp = request.FILES.get('dp')
+    if request.method == "POST":
+        user_dp = request.FILES.get("dp")
         if check_image_extension(user_dp.name):
             profile_form = Profile.objects.get(user=request.user)
             profile_form.dp = user_dp
@@ -210,26 +204,28 @@ def change_picture(request):
                 </p>
             """
             messages.warning(request, msg_txt)
-            return redirect('picture_change')
+            return redirect("picture_change")
 
-        return redirect('user_profile', username=request.user.username)
+        return redirect("user_profile", username=request.user.username)
     else:
         profile_form = ProfileEditForm(instance=request.user.profile)
-        return render(request, 'users/change_picture.html', {'profile_form': profile_form})
+        return render(
+            request, "users/change_picture.html", {"profile_form": profile_form}
+        )
 
 
 class UserProfilePageView(LoginRequiredMixin, ListView):
     """
     Basic ListView implementation to call the subjects list & profile per user.
     """
+
     model = Subject
     paginate_by = 15
-    template_name = 'users/profile.html'
-    context_object_name = 'subjects'
+    template_name = "users/profile.html"
+    context_object_name = "subjects"
 
     def get_queryset(self, **kwargs):
-        self.user = get_object_or_404(User,
-                                      username=self.kwargs['username'])
+        self.user = get_object_or_404(User, username=self.kwargs["username"])
         return Subject.get_subjects(self.user)
 
     def get_context_data(self, *args, **kwargs):
@@ -249,11 +245,13 @@ def send_message_request(request, user_id):
 
     if contacter in receiver.profile.pending_list.all():
         receiver.profile.pending_list.remove(contacter)
-        text = 'Send Request'
+        text = "Send Request"
     else:
         receiver.profile.pending_list.add(contacter)
-        Notification.objects.create(Actor=contacter, Target=receiver, notif_type='sent_msg_request')
-        text = 'Request Sent'
+        Notification.objects.create(
+            Actor=contacter, Target=receiver, notif_type="sent_msg_request"
+        )
+        text = "Request Sent"
     return HttpResponse(text)
 
 
@@ -270,10 +268,12 @@ def accept_message_request(request, user_id):
         acceptor.profile.pending_list.remove(sender)
         acceptor.profile.contact_list.add(sender)
         sender.profile.contact_list.add(acceptor)
-        Notification.objects.create(Actor=acceptor, Target=sender, notif_type='confirmed_msg_request')
-        text = 'Added to contact list'
+        Notification.objects.create(
+            Actor=acceptor, Target=sender, notif_type="confirmed_msg_request"
+        )
+        text = "Added to contact list"
     else:
-        text = 'Unexpected error!'
+        text = "Unexpected error!"
     return HttpResponse(text)
 
 
@@ -285,7 +285,7 @@ def all_message_requests(request):
     message_requests = request.user.profile.pending_list.all()
 
     paginator = Paginator(message_requests, 20)
-    page = request.GET.get('page')
+    page = request.GET.get("page")
     if paginator.num_pages > 1:
         p = True
     else:
@@ -301,12 +301,11 @@ def all_message_requests(request):
 
     p_obj = users
 
-    return render(request, 'users/view_all_message_requests.html', {
-        'users': users,
-        'page': page,
-        'p': p,
-        'p_obj': p_obj
-    })
+    return render(
+        request,
+        "users/view_all_message_requests.html",
+        {"users": users, "page": page, "p": p, "p_obj": p_obj},
+    )
 
 
 @login_required
@@ -317,7 +316,7 @@ def all_friends(request):
     user_contact_list = request.user.profile.contact_list.all()
 
     paginator = Paginator(user_contact_list, 20)
-    page = request.GET.get('page')
+    page = request.GET.get("page")
     if paginator.num_pages > 1:
         p = True
     else:
@@ -333,24 +332,24 @@ def all_friends(request):
 
     p_obj = users
 
-    return render(request, 'users/view_all_contacts.html', {
-        'users': users,
-        'page': page,
-        'p': p,
-        'p_obj': p_obj
-    })
+    return render(
+        request,
+        "users/view_all_contacts.html",
+        {"users": users, "page": page, "p": p, "p_obj": p_obj},
+    )
+
 
 @unauthenticated_user
 def user_login(request):
-    context = {}    
-    if request.method == 'POST':
-        email = request.POST.get('username')
-        password = request.POST.get('password')
+    context = {}
+    if request.method == "POST":
+        email = request.POST.get("username")
+        password = request.POST.get("password")
         user = authenticate(request, username=email, password=password)
         if user is not None:
             auth_login(request, user)
-            return redirect('home')
+            return redirect("home")
         else:
             messages.success(request, "Login Failed! Try Again")
-            return redirect('login')
-    return render(request, 'registration/login.html', context)
+            return redirect("login")
+    return render(request, "registration/login.html", context)
